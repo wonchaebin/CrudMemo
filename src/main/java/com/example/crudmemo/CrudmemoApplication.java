534f4fd19062f267e0af130f3723e2414beb03cf
wonchaebin <usan119@naver.com>
2025-02-07 19:26:18 +0900

    Initial commit

---
+++ src/main/java/com/example/crudmemo/CrudmemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudmemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudmemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudmemoApplication.class, args);
+    }
+
+}
